81124df3ad6bfb6210eb70fd207495783a7112db
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-10-16 17:54:04 +0300

    task4-comments

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ColorDirectiveDirective} from './color-directive.directive';
+import { ColorDirectiveDirective } from './color-directive.directive';
 import { OrderByPipe } from './order-by.pipe';
 
+const sharedItems = [ColorDirectiveDirective, OrderByPipe];
+
 @NgModule({
-  declarations: [ColorDirectiveDirective, OrderByPipe],
-  imports: [
-    CommonModule
-  ],
-  exports: [ColorDirectiveDirective, OrderByPipe]
+  declarations: [...sharedItems],
+  imports: [CommonModule],
+  exports: [...sharedItems]
 })
-export class SharedModuleModule { }
+export class SharedModuleModule {}
